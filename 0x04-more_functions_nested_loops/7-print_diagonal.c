1f4c791b598811431b86ba338df5d59baa0ad6f2
Kolade jerry <jkolade746@gmail.com>
2023-02-24 21:03:03 -0800

    diagonals

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -13,11 +13,14 @@ void print_diagonal(int n)
 		int i, j;
 		for (i = 0; i < n; i++)
 		{
-			if (j == i)
-				_putchar('\\');
-			else if (j < i)
-				_putchar(' ');
+			for (j = 0; j < n; j++)
+			{
+				if (j == i)
+					_putchar('\\');
+				else if (j < i)
+					_putchar(' ');
+			}
+			_putchar('\n');
 		}
-		_putchar('\n');
 	}
 }
