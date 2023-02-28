60e5a2c1989d71a410e98ce80e1c54f7f49c15c6
Kolade jerry <jkolade746@gmail.com>
2023-02-27 20:30:57 -0800

    This is a lot streesful

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 /**
- *A c program
- *_puts: function to print the str followed by a new line to the stdout
- *@str: string
+ * A c program
+ * _puts - function to print the str followed by a new line to the stdout
+ * @str: string
  */
 
 void _puts(char *str)
