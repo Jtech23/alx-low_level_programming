858905b72eaff9e080c85c8c454bbc2d7958d42b
Kolade jerry <jkoade746@gmail.com>
2023-08-16 19:54:28 -0700

    going

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * times_table - Prints table 9
+ * Return: Always 0
+ */
+
+void times_table(void)
+{
+	int a, b;
+
+	b = 0;
+	for (a = 0; a <= 9; a++)
+	{
+		_putchar(a * 0 );
+		_putchar('\n');
+	}
+	for (a = 0; a <= 9; a++)
+	{
+		_putchar(a);
+		_putchar('\n');
+	}
+	while (b <= 9)
+	{
+		_putchar(48);
+		_putchar(c * 2)
+		c++
+		b++
+	}
+}
