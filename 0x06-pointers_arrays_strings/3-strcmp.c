e045bc7c8cd649171f0d9299187106bef1739429
Kolade jerry <jkoade746@gmail.com>
2023-08-24 19:21:44 -0700

    completed

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -3,13 +3,14 @@
  * _strcmp - compares two strings
  * @s1: STring ont to copare
  * @s2: String 2 to comapre
+ * Return: Always 0
  */
 
 int _strcmp(char *s1, char *s2)
 {
 	int i, j;
 
-	j = 0 ;
+	j = 0;
 	for (i = 0; s1[i] != '\0' && j == 0; i++)
 	{
 		j = s1[i] - s2[i];
