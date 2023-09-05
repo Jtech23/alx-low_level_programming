102d3736bc02707ae44f8b233bcdd124deaec574
Kolade jerry <Jkolade746@gmail.com>
2023-09-05 03:05:26 -0700

    completed

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * @argc: argument count
+ * @argv: Argument variable
+ * Return: Always 0
+ */
+
+int main(int argc, char *argv[])
+{
+	printf(argv[0]);
+	printf("\n");
+	return(0);
+}
