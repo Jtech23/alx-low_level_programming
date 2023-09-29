f9e3322883df5e310061ce46f72398a7249a0aa5
Jerry kolade <jkolade746@gmail.com>
2023-09-29 03:31:25 -0700

    2 completed

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _strlen_recursion - Prints string length
+ * @s: string to prints its length
+ * return: returns the value of a
+ */
+
+int _strlen_recursion(char *s)
+{
+	int a = 0;
+
+	if (*s != '\0')
+	{
+		a++;
+		a +=_strlen_recursion(s + 1);
+	}
+	return (a);
+}
