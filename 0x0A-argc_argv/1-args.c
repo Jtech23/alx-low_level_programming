345fd2546fe782712769087278f439ab776a8065
Jerry kolade <jkolade746@gmail.com>
2023-10-04 05:10:58 -0700

    completed task 2

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - entry point
+ * @argc: argument count
+ * Return: always 0
+ */
+
+int main(int argc, char *argv[])
+{
+(void) argv;
+	printf("%d", argc - 1);
+	printf("\n");
+	return (0);
+}
